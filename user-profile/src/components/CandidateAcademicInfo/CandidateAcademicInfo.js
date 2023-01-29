86a2f6c6a4691a4d74ae9f78ac19f5110bb759cf
Mufaddal <mufaddal52102@gmail.com>
2023-01-29 12:29:13 +0500

    added table view & add more data functionality

--- user-profile/src/components/CandidateAcademicInfo/CandidateAcademicInfo.js
+++ user-profile/src/components/CandidateAcademicInfo/CandidateAcademicInfo.js
@@ -4,7 +4,7 @@ import InputField from "../InputField/InputField"
 import PhoneComponent from "../PhoneComponent/PhoneComponent";
 import Button from "../Button/Button";
 import Heading from "../Heading/Heading";
-import styles from '../CandidatePersonalInfo/CandidatePersonalInfo.module.css'
+import styles from '../CandidateAcademicInfo/CandidateAcademicInfo.module.css'
 import { AlertMessage } from "../AlertMessage/AlertMessage";
 import TextArea from "../Textarea/TextArea";
 // import AddCircleIcon from '@material-ui/icons/AddCircle';
@@ -14,18 +14,39 @@ function CandidateAcademicInfo() {
     const [title, setTitle] = useState('');
     const [institute, setInstitute] = useState('');
     const [degreeProgress, setDegreeProgress] = useState(false);
-    const [graduationDate, setGraduationDate] = useState('2015-01-01');
+    const [graduationDate, setGraduationDate] = useState('');
     const [percentage, setPercentage] = useState('');
     const [fypThesis, setFypThesis] = useState('')
+
+    const [eduData, setEduData] = useState([]);
+
+    const [view, setView] = useState('details')
+
+    const [disGradDate, setDisGradDate] = useState(false);
+    const [disNextBtn, setDisNextBtn] = useState(true);
 
     const [showAlert, setShowAlert] = useState(false);
     const [alertType, setAlertType] = useState('alert');
     const [message, setMessage] = useState('');
     const [disableSubmitBtn, setDisableSubmitBtn] = useState(true);
 
+    const url = ''
     useEffect(() => {
-
-    })
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setEduData(data)
+                if(data.length !== 0){
+                    setDisNextBtn(false)
+                }
+            })
+            .catch(err => {
+                console.log(err, "\nhello I caught this error");
+                setEduData([]);
+                setDisNextBtn(true)
+            });
+    }, [])
 
     const handleDegree = useCallback(val => {
         console.log(val);
@@ -37,11 +58,22 @@ function CandidateAcademicInfo() {
     const handleInstitute = useCallback(val => {
         setInstitute(val)
     }, []);
-    const handleDegreeProgress = useCallback(val => {
+    const handleDegreeProgress = useCallback(e => {
+        const val = e.target.value
+        console.log(val)
         setDegreeProgress(val);
+        if (val === 'yes') {
+            setDisGradDate(true)
+            setGraduationDate('')
+        }
+        else if (val === 'no') {
+            setDisGradDate(false)
+        }
+
     }, []);
     const handleGraduationDate = useCallback(val => {
         setGraduationDate(val);
+        setDegreeProgress('no');
     }, []);
     const handlePercentage = useCallback(val => {
         setPercentage(val);
@@ -50,53 +82,160 @@ function CandidateAcademicInfo() {
         setFypThesis(val);
     }, []);
 
+    const onAddAnother = useCallback(() => {
+        setView('add field')
+    }, []);
+
+    const onCancel = useCallback(() => {
+        setDegree('')
+        setTitle('')
+        setInstitute('')
+        setDegreeProgress('')
+        setGraduationDate('')
+        setPercentage('')
+        setFypThesis('')
+        setDisGradDate(false)
+        setView('details')
+    }, []);
 
 
-    const onSubmit = () => { }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const obj = {
+            degree,
+            title,
+            institute,
+            degreeProgress,
+            graduationDate,
+            percentage,
+            fypThesis
+        }
+        setDegree('')
+        setTitle('')
+        setInstitute('')
+        setDegreeProgress('')
+        setGraduationDate('')
+        setPercentage('')
+        setFypThesis('')
+        setDisGradDate(false)
+        setEduData([...eduData, obj])
+        setView('details')
+    }
 
-    return (
-        <>
-            <div className={styles.mainContainer}>
-                <form className={styles.formPersonalInfo} onSubmit={onSubmit}>
+    if (view == 'details') {
+        return (
+            <div>
+                <div>
+                    <table className={styles.eduTable}>
+                        <tr>
+                            <th>Qualification</th>
+                            <th>Title</th>
+                            <th>School/University/College</th>
+                            <th>In progress</th>
+                            <th>Completion date</th>
+                            <th>CGPA/Percentage</th>
+                            <th>FYP/Project/Thesis</th>
+                        </tr>
+                        {eduData.length === 0 ? <tr>
+                            <td style={{ textAlign: 'center' }} colSpan={7}>No data to show</td>
+                        </tr> : null}
+                        {
+                            eduData.map(item => {
+                                console.log(item)
+                                return (
+                                    <tr>
+                                        {
+                                            Object.keys(item).map(it => {
+                                                return <td>{item[it]}</td>
+                                            })
+                                        }
+                                    </tr>
+                                )
+                            })
+                        }
+                    </table>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'center'}}>
+                    <Button onClick={onAddAnother} disabled={''} text="+ Add Another" type="button" className={styles.addMoreBtn} />
+                </div>
+                <div>
+                    <Button text="Save" type="button" className={styles.saveButton} />
+                    <Button disabled={disNextBtn} text="Next" type="button" className={styles.nextButton} />
+                </div>
+            </div>
+        )
+    }
+    else if (view == 'add field') {
+        return (
+            <>
+                <div className={styles.mainContainer}>
+                    <form className={styles.formPersonalInfo} onSubmit={onSubmit}>
 
-                    {showAlert ? <AlertMessage showAlert={showAlert} setAlert={setShowAlert} alertType={alertType} message={message} /> : ''}
-                    <Heading className={styles.personalInfoHeading} text="Academic Background" />
-                    <div>
-                        <DropdownField value={degree} handler={handleDegree} options={['Secondary School Certificate / Matriculation / O - level', 'Higher Secondary School Certificate / Intermediate/ A - level', 'Bachelor (16 Years) Degree', 'Master (16 Years) Degree']} className={styles.fullSize} placeholder='Qualification Level' />
-                    </div>
-                    <div>
-                        <InputField value={title} handler={handleTitle} type='text' placeholder='Ttile' pattern="[a-zA-Z ]*" className={styles.fullSize} required='required' />
-                    </div>
-
-                    <div>
-                        <InputField value={institute} handler={handleInstitute} type='text' placeholder='University/Collage' pattern="[a-zA-Z ]*" className={styles.fullSize} required='required' />
-                    </div>
-                    <div className={styles.degreeProgressDiv}>
-                        <div><InputField value={degreeProgress} handler={handleDegreeProgress} id='degree-in-progress' type='radio' placeholder='' className={styles.radioBtn} required='required' />
-                            <span className={styles.degreeProgressText}>Degree in progress</span>
+                        {showAlert ? <AlertMessage showAlert={showAlert} setAlert={setShowAlert} alertType={alertType} message={message} /> : ''}
+                        <Heading className={styles.personalInfoHeading} text="Academic Background" />
+                        <div>
+                            <DropdownField value={degree} handler={handleDegree} options={['Secondary School Certificate / Matriculation / O - level', 'Higher Secondary School Certificate / Intermediate/ A - level', 'Bachelor (16 Years) Degree', 'Master (16 Years) Degree']} className={styles.fullSize} placeholder='Qualification Level' />
                         </div>
                         <div>
-                            <span className={styles.degreeProgressText}>Completaion date: </span>
-                            <InputField value={graduationDate} handler={handleGraduationDate} min="" type='date' placeholder='' className={styles.graduationDate} required='required' />
+                            <InputField value={title} handler={handleTitle} type='text' placeholder='Title' pattern="[a-zA-Z ]*" className={styles.fullSize} required='required' />
                         </div>
 
-                    </div>
-                    <div>
-                        <InputField value={percentage} handler={handlePercentage} type='text' pattern="[0-9.%]*" placeholder='CGPA/Percentage' className={styles.fullSize} required='required' />
-                    </div>
-                    <div>
-                        <TextArea value={fypThesis} handler={handleFypthesis} rows="5" placeholder="Final Year Project or Thesis (If applicable)" className={styles.textArea} />
-                    </div>
-                    <div>
-                        <Button disabled={''} text="+ Add Another" type="button" className={styles.addMoreBtn} />
-                        <Button type="submit" text="Submit" className={styles.saveButton} />
-                    </div>
+                        <div>
+                            <InputField value={institute} handler={handleInstitute} type='text' placeholder='University/College' pattern="[a-zA-Z ]*" className={styles.fullSize} required='required' />
+                        </div>
+                        <div className={styles.degreeProgressDiv}>
+                            <div>
+                                <span className={styles.degreeProgressText}>Degree in progress:</span>
+                                <input
+                                    checked={degreeProgress === 'yes'}
+                                    value={'yes'}
+                                    onChange={handleDegreeProgress}
+                                    type={'radio'}
+                                    className={`${styles.radioBtn} ${styles.inputFields}`}
+                                    required={true}
+                                />
+                                <span>Yes</span>
+                                <input
+                                    checked={degreeProgress === 'no'}
+                                    value={'no'}
+                                    onChange={handleDegreeProgress}
+                                    type={'radio'}
+                                    className={`${styles.radioBtn} ${styles.inputFields}`}
+                                    required={true}
+                                />
+                                <span>No</span>
+                                {/* <InputField 
+                                value={'yes'}
+                                checked={degreeProgress === 'yes'}
+                                handler={handleDegreeProgress} 
+                                id='degree-in-progress' 
+                                type='radio' 
+                                placeholder='' 
+                                className={styles.radioBtn} 
+                                required='required' 
+                            /> */}
+                            </div>
+                            <div>
+                                <span className={styles.degreeProgressText}>Completion date: </span>
+                                <InputField disabled={disGradDate} value={graduationDate} handler={handleGraduationDate} min="" type='date' placeholder='' className={styles.graduationDate} required='required' />
+                            </div>
 
+                        </div>
+                        <div>
+                            <InputField value={percentage} handler={handlePercentage} type='text' pattern="[0-9.%]*" placeholder='CGPA/Percentage' className={styles.fullSize} required='required' />
+                        </div>
+                        <div>
+                            <TextArea value={fypThesis} handler={handleFypthesis} rows="5" placeholder="Final Year Project or Thesis (If applicable)" className={styles.textArea} />
+                        </div>
+                        <div>
+                            <Button type="submit" text="Add" className={styles.saveButton} />
+                            <Button onClick={onCancel} disabled={''} text="Cancel" type="button" className={styles.saveButton} />
+                        </div>
+                    </form>
+                </div>
+            </>
 
-                </form>
-            </div>
-        </>
-
-    )
+        )
+    }
 }
 export default CandidateAcademicInfo;
