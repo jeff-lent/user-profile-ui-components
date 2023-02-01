b8e0079a0c1f34c4a57ef4343d43f9343af12676
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2023-02-01 09:06:28 +0500

    Edit & delete functionality done

--- user-profile/src/components/CandidateAcademicInfo/CandidateAcademicInfo.js
+++ user-profile/src/components/CandidateAcademicInfo/CandidateAcademicInfo.js
@@ -28,7 +28,7 @@ function CandidateAcademicInfo() {
     const [graduationDate, setGraduationDate] = useState('');
     const [percentage, setPercentage] = useState('');
     const [fypThesis, setFypThesis] = useState('')
-    const [userId, setUserId] = useState(2);
+    const [userId, setUserId] = useState(91);
     //setUserId(sessionStorage.getItem("user_id")),
 
     const [eduData, setEduData] = useState([]);
@@ -41,9 +41,16 @@ function CandidateAcademicInfo() {
     const [showAlert, setShowAlert] = useState(false);
     const [alertType, setAlertType] = useState('alert');
     const [message, setMessage] = useState('');
+    const [showDelete, setShowDelete] = useState(false);
+    const [showEdit, setShowEdit] = useState(false);
+    const [deleteId, setDeleteId] = useState(null);
+    const [editId, setEditId] = useState(null)
 
-    const getByUserIdUrl = 'http://192.168.0.160:8080/api/educational_information/user'
-    const postUrl = 'http://192.168.0.160:8080/api/educational_information'
+    const basicRoute = 'http://192.168.0.160:8080/api/educational_information'
+    const getByUserIdUrl = `${basicRoute}/user`
+    const postUrl = basicRoute
+    const deleteUrl = basicRoute
+    const putUrl = basicRoute
 
     useEffect(() => {
         fetchData()
@@ -100,6 +107,8 @@ function CandidateAcademicInfo() {
 
     const onAddAnother = useCallback(() => {
         setView('add field')
+        setShowDelete(false)
+        setDeleteId(null)
     }, []);
 
     const onCancel = useCallback(() => {
@@ -112,6 +121,8 @@ function CandidateAcademicInfo() {
         setFypThesis('')
         setDisGradDate(false)
         setView('details')
+        setShowEdit(false)
+        setEditId(null)
     }, []);
 
     const onSubmit = (e) => {
@@ -176,9 +187,124 @@ function CandidateAcademicInfo() {
         setView('details')
     }
 
+    const onDelete = (id) => {
+        setShowDelete(true);
+        setDeleteId(id);
+    }
+
+    const deleteField = () => {
+        fetch(`${deleteUrl}/${deleteId}`, {
+            method: 'DELETE',
+        })
+            .then(res => console.log(res))
+        const newData = eduData.filter((item) => item.id !== deleteId);
+        setEduData(newData)
+        setShowDelete(false);
+        setDeleteId(null);
+    }
+
+    const dontDeleteField = () => {
+        setShowDelete(false);
+        setDeleteId(null);
+    }
+
+    const onEdit = (obj) => {
+        setShowDelete(false)
+        setDeleteId(null)
+
+        setView('add field')
+        setShowEdit(true)
+        setEditId(obj.id)
+
+        setDegree(obj.currentDegree)
+        setTitle(obj.title)
+        setInstitute(obj.institute)
+        setDegreeProgress(obj.degreeProgress)
+        if(obj.degreeProgress === "No"){
+            setGraduationDate(obj.graduationDate)
+        }
+        else {
+            setDisGradDate(true)
+            setGraduationDate("")
+        }
+        setPercentage(obj.cgpa)
+        setFypThesis(obj.finalYearProject)
+    }
+
+    const editField = () => {
+        const obj = {
+            id: editId,
+            userId,
+            currentDegree: degree,
+            title,
+            institute,
+            degreeProgress,
+            graduationDate,
+            cgpa: percentage,
+            finalYearProject: fypThesis
+        }
+        fetch(`${postUrl}/${editId}`, {
+            method: 'PUT',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(obj)
+        })
+            .then(async response => {
+                const data = await response.json();
+                console.log(response);
+                const res = response ? response.ok : false;
+                const updateUser = res ? 'Info edited successfully!' : 'Error editing info!';
+                const alertUser = res ? 'success' : 'danger';
+                setShowAlert(true);
+                setMessage(updateUser)
+                setAlertType(alertUser)
+                setDisNextBtn(!res)
+                // swal({
+                //     title: "Personal Information Saved!",
+                //     icon: "success",
+                // })
+            })
+            .catch(err => {
+                console.log(err)
+                const updateUser = 'Error editing info!';
+                const alertUser = 'danger';
+                setShowAlert(true);
+                setMessage(updateUser)
+                setAlertType(alertUser)
+                setDisNextBtn(true)
+            });
+
+        const newData = eduData.map((item) => {
+            if(item.id === editId){
+                return obj
+            }
+            else
+            return item;
+        })
+        setEduData(newData)
+        setDegree('')
+        setTitle('')
+        setInstitute('')
+        setDegreeProgress('')
+        setGraduationDate('')
+        setPercentage('')
+        setFypThesis('')
+        setDisGradDate(false)
+        setView('details')
+
+        setEditId(null)
+        setShowEdit(false)
+    }
+
     if (view == 'details') {
         return (
-            <div className={styles.mainContainer} style={{display: 'block'}}>
+            <div className={styles.mainContainer} style={{ display: 'block' }}>
                 {showAlert ? <AlertMessage showAlert={showAlert} setAlert={setShowAlert} alertType={alertType} message={message} /> : ''}
                 <Heading className={styles.personalInfoHeading} text="Academic Background" />
                 <div className={styles.tableContainer}>
@@ -191,14 +317,15 @@ function CandidateAcademicInfo() {
                             <th>Completion date</th>
                             <th>CGPA / Percentage</th>
                             <th>FYP / Project / Thesis</th>
+                            <th>Action</th>
                         </tr>
                         {eduData.length === 0 ? <tr>
-                            <td style={{ textAlign: 'center' }} colSpan={7}>No data to show</td>
+                            <td style={{ textAlign: 'center' }} colSpan={8}>No data to show</td>
                         </tr> : null}
                         {
                             eduData.map(item => {
                                 return (
-                                    <tr key={item.id}>
+                                    <tr key={item.id} style={item.id === deleteId ? {color: '#fa928e'} : {}}>
                                         {
                                             Object.keys(item).map(it => {
                                                 if (it !== 'userId' && it !== 'id')
@@ -208,15 +335,23 @@ function CandidateAcademicInfo() {
                                                         return <td>{item[it]}</td>;
                                             })
                                         }
+                                        <td>
+                                            <Button onClick={() => onDelete(item.id)} type="button" text={<i class="fa fa-trash"></i>} />
+                                            <Button onClick={() => onEdit(item)} type="button" text={<i class="fas fa-edit"></i>} />
+                                        </td>
                                     </tr>
                                 )
                             })
                         }
                     </table>
                 </div>
-                {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Button onClick={onAddAnother} disabled={''} text="+ Add Another" type="button" className={styles.addMoreBtn} />
-                </div> */}
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    {showDelete && <div style={{backgroundColor: '#fa928e', border: '2px solid red', padding: '10px', borderRadius: '10px'}} >
+                        Are you sure you want to delete this?
+                        <Button text="Yes" onClick={deleteField} />
+                        <Button text="No" onClick={dontDeleteField} />
+                    </div>}
+                </div>
                 <div>
                     <Button onClick={onAddAnother} text="+ Add New" type="button" className={styles.saveButton} />
                 </div>
@@ -278,7 +413,11 @@ function CandidateAcademicInfo() {
                             <TextArea value={fypThesis} handler={handleFypthesis} rows="5" placeholder="Final Year Project or Thesis (If applicable)" className={styles.textArea} />
                         </div>
                         <div>
-                            <Button type="submit" text="Add" className={styles.saveButton} />
+                            {
+                                showEdit ?
+                                <Button onClick={editField} type="button" text="Edit" className={styles.saveButton} /> :
+                                <Button type="submit" text="Add" className={styles.saveButton} />
+                            }
                             <Button onClick={onCancel} disabled={''} text="Cancel" type="button" className={styles.saveButton} />
                         </div>
                     </form>
